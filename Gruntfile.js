8df049023446ebe2f41b6972338db4f9aa251dec
Dan Kang <dan@dskang.com>
2013-08-08 10:09:02 -0400

    add grunt cucumber

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,16 @@ module.exports = function(grunt) {
           base: 'app'
         }
       }
+    },
+
+    cucumberjs: {
+      files: 'features/*.feature'
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-cucumber');
 
   grunt.registerTask('server', ['connect:server']);
+  grunt.registerTask('test', ['cucumberjs']);
 };
